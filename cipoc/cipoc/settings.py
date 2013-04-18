20d8dd988485ec2e84e622f2cafe3f562dc5a54a
Mirmillou <mirmillou@online.fr>
2013-04-18 23:44:08 +0200

    Première vue et base de données Hello

--- cipoc/cipoc/settings.py
+++ cipoc/cipoc/settings.py
@@ -1,4 +1,6 @@
 # Django settings for cipoc project.
+import os
+CWD = os.getcwd()
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,12 +11,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-import os
-CWD = os.getcwd()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': CWD+'/db',                      # Or path to database file if using sqlite3.
+#        'NAME': CWD+'/db',                      # Or path to database file if using sqlite3.
+        'NAME': 'cipoc.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -113,6 +115,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    CWD
 )
 
 INSTALLED_APPS = (
--- cipoc/hello/views.py
+++ cipoc/hello/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+from django import template
+from django.template import Context
+from django.http import HttpResponse
+from hello.models import Hello
+ 
+def index(request):
+    hs = Hello.objects.all()
+    t = template.loader.get_template('hello/index.html')
+    c = Context({ 
+        "hello_list" : hs
+    })
+    return HttpResponse(t.render(c))
+
