61383539fef2c050f5be9b75708c93c2d48db4f1
Mirmillou <mirmillou@online.fr>
2013-04-20 23:16:55 +0200

    Added List/Detail generic view

--- cipoc/hello/urls.py
+++ cipoc/hello/urls.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, include, url
+from django.views.generic import ListView, DetailView
+from hello.models import Friend
 
 urlpatterns = patterns('',
-    url(r'^$', 'hello.views.index'),
+#    url(r'^$', 'hello.views.index'),
+    url(r'^$', ListView.as_view(
+        model=Friend,
+    ), name='friends'),
 
-
+    url(r'^friend/(?P<pk>\d+)/$',
+      DetailView.as_view(
+        model=Friend,
+        template_name='hello/friend_detail.html'), name="friend"),
 )
+     
